1ea1dfff7d0d9e18e331bbbd7c16982b72f1d8be
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-21 16:32:27 +0700

    feat: add cud handler member

--- controllers/member.js
+++ controllers/member.js
@@ -1,5 +1,7 @@
+const { sequelize } = require('../models');
 const { memberSvc } = require('../services');
 const paginate = require('../utils/generate_pagination');
+const err = require('../utils/errors');
 
 module.exports = {
     index: async (req, res, next) => {
@@ -21,6 +23,78 @@ module.exports = {
                 message: 'Members data retrieved successfully',
                 pagination,
                 data: members.rows,
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    create: async (req, res, next) => {
+        let transaction;
+        try {
+            const body = req.body;
+            const { name, email, phone } = body;
+
+            transaction = await sequelize.transaction();
+            const member = await memberSvc.addMember(name, email, phone);
+
+            await transaction.commit();
+            return res.status(201).json({
+                status: 'OK',
+                message: 'Member added successfully',
+                data: member
+            });
+        } catch (error) {
+            if (transaction) await transaction.rollback();
+            next(error);
+        }
+    },
+
+    update: async (req, res, next) => {
+        let transaction;
+        try {
+            const body = req.body;
+            const { member_code } = req.params;
+            const { name, email, phone } = body;
+
+            let member = await memberSvc.getMemberByCode(member_code);
+            if (!member) return err.not_found(res, "Member not found");
+
+            transaction = await sequelize.transaction();
+            member = await memberSvc.updateMember(
+                member.code,
+                name || member.name,
+                email || member.email,
+                phone || member.phone
+            );
+
+            member = await memberSvc.getMemberByCode(member_code);
+
+            await transaction.commit();
+            return res.status(200).json({
+                status: 'OK',
+                message: 'Member updated successfully',
+                data: member
+            });
+        } catch (error) {
+            if (transaction) await transaction.rollback();
+            next(error);
+        }
+    },
+
+    delete: async (req, res, next) => {
+        try {
+            const { member_code } = req.params;
+
+            let member = await memberSvc.getMemberByCode(member_code);
+            if (!member) return err.not_found(res, "Member not found");
+
+            await memberSvc.deleteMember(member.code);
+
+            return res.status(200).json({
+                status: 'OK',
+                message: 'Member deleted successfully',
+                data: null,
             });
         } catch (error) {
             next(error);
--- routes/member.js
+++ routes/member.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const { member } = require('../controllers');
 
 router.get('/', member.index);
+router.post('/', member.create);
+router.put('/:member_code', member.update);
+router.delete('/:member_code', member.delete);
 
 module.exports = router;
