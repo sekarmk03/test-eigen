ff4cca77e6c5920e3035d0bf6e945dc70095413d
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-21 13:33:55 +0700

    feat: add book crud services

--- services/book.js
+++ services/book.js
@@ -42,7 +42,7 @@ module.exports = {
                 {
                     model: Publisher,
                     as: 'publisher',
-                    attributes: ['id', 'name']
+                    attributes: ['id', 'name', 'address']
                 }
             ],
             limit: limit,
@@ -54,4 +54,45 @@ module.exports = {
 
         return books;
     },
+
+    addBook: async (title, authorId, stock, isbn, publisherId, publishedDate) => {
+        const book = await Book.create({
+            title: title,
+            author_id: authorId,
+            stock_total: stock,
+            stock_available: stock,
+            isbn: isbn,
+            publisher_id: publisherId,
+            published_date: publishedDate
+        });
+
+        return book;
+    },
+
+    updateBook: async (bookCode, title, authorId, stock, isbn, publisherId, publishedDate) => {
+        const updated = await Book.update({
+            title: title,
+            author_id: authorId,
+            stock_total: stock,
+            isbn: isbn,
+            publisher_id: publisherId,
+            published_date: publishedDate
+        }, {
+            where: {
+                code: bookCode
+            }
+        });
+
+        return updated;
+    },
+
+    deleteBook: async (bookCode) => {
+        const deleted = await Book.destroy({
+            where: {
+                code: bookCode
+            }
+        });
+
+        return deleted;
+    }
 }
